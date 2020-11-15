9e8f446deb1f4ece3bd7a690cae9e8426b3cb34d
essasabbagh <essasabbagh@gmail.com>
2020-11-15 14:11:46 +0300

    vue start

--- main.js
+++ main.js
@@ -8,4 +8,30 @@ console.log(essa());
 
 let myStr = `Hi bitch ${essa()}`
 
-console.log(myStr)
+console.log(myStr)  
+
+const myObj = {
+    name : `essa`,
+    age : 32,
+    city : `istanbul`,
+    skill : { html : `70%`,
+            css : `60%`,
+            js : [`vuejs`, `reactjs`]}
+}
+
+// const {name, age ,city ,skill: {html , css ,js:[one ,two]}} = myObj;
+function infoShow ({name, age ,city ,skill: {html , css ,js:[one ,two]}}) {
+
+    console.log(`My name is ${name},I have ${age}, and I live in the ${city}, 
+            My skill progrras in HTML is ${html}, and in CSS is ${css}, and I know ${one},and ${two}, framwork `);
+}
+
+infoShow(myObj);
+
+
+new Vue ({
+    el: `#ESSA`,
+    data: {
+        message: `hi`
+    }
+})
